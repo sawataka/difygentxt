4890aea1f7b6e39ec53f23c271585fd7dc79fb97
sawataka <sawataka2008@gmail.com>
2026-03-12 02:51:38 +0900

    add bg version

--- static/js/app.js
+++ static/js/app.js
@@ -1,43 +1,81 @@
 const form = document.getElementById("form")
 
+const messages = [
+ "AIが質問を分析しています...",
+ "情報を整理しています...",
+ "回答を生成しています..."
+]
+
+let i = 0
+
+function startLoadingAnimation(){
+
+ const text = document.getElementById("loading-text")
+
+ setInterval(()=>{
+
+  text.innerText = messages[i]
+
+  i = (i+1)%messages.length
+
+ },2000)
+
+}
+
 form.addEventListener("submit", async function(e){
 
  e.preventDefault()
 
- const loading = document.getElementById("loading")
- const result = document.getElementById("result")
+ document.getElementById("loading").style.display="block"
 
- loading.style.display = "block"
- result.style.display = "none"
+ document.getElementById("result").style.display="none"
+
+ startLoadingAnimation()
 
  const formData = new FormData(form)
 
  const res = await fetch("/generate",{
+
   method:"POST",
   body:formData
+
  })
 
  const data = await res.json()
 
- loading.style.display = "none"
+ document.getElementById("loading").style.display="none"
 
- if(data.error){
-  alert(data.error)
-  return
- }
+ showResult(data)
 
- document.getElementById("description").innerHTML =
-  data.description
+})
+
+function showResult(data){
+
+ document.getElementById("result").style.display="block"
+
+ const temp = document.createElement("div")
+ temp.innerHTML = data.description
+
+ const text = temp.innerText
+
+ typeWriter("description", text)
+
+
+//  typeWriter(
+//   "description",
+//   data.description
+//  )
 
  const pointsDiv = document.getElementById("points")
- pointsDiv.innerHTML = ""
 
- data.points.forEach(p => {
+ pointsDiv.innerHTML=""
+
+ data.points.forEach(p=>{
 
   const card = document.createElement("div")
 
   card.className =
-   "bg-blue-50 p-4 rounded shadow"
+  "bg-gray-800 p-6 rounded shadow"
 
   card.innerText = p
 
@@ -45,13 +83,46 @@ form.addEventListener("submit", async function(e){
 
  })
 
- document.getElementById("sns").innerText =
-  data.sns_post
+ document.getElementById("sns").innerText = data.sns_post
 
- result.style.display = "block"
 
+ // 👇ここでびよーんスクロール
+document.getElementById("result").scrollIntoView({
+  behavior: "smooth"
 })
 
+
+}
+
+function typeWriter(id,html){
+
+ const el = document.getElementById(id)
+
+ el.innerHTML=""
+
+ let i=0
+
+ const speed=10
+
+ const txt = html
+
+ function typing(){
+
+  if(i<txt.length){
+
+   el.innerHTML += txt.charAt(i)
+
+   i++
+
+   setTimeout(typing,speed)
+
+  }
+
+ }
+
+ typing()
+
+}
 
 function copyPost(){
 
